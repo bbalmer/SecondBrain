8d61c4110501a535d93060e0c3cea4aa04346229
Brad Balmer <bbalmer@laughlin.com>
2016-05-12 23:36:38 -0500

    Initial creation of project

---
+++ SecondBrain/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SecondBrain
+//
+//  Created by Brad Balmer on 5/12/16.
+//  Copyright © 2016 Remlab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
